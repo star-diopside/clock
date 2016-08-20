8645dafe4ab4ee5055fde8002aff67955cde4ba6
se-yoshi <se.yoshi@gmail.com>
2016-08-21 05:07:57 +0900

    プロジェクト新規作成

---
+++ src/main/java/jp/gr/java_conf/star_diopside/clock/application/App.java
@@ -0,0 +1,16 @@
+package jp.gr.java_conf.star_diopside.clock.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
